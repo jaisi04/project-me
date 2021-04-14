3abfecc02f4302e7ba8b6b63592b5ef25fc95acf
Akshay Jaiswal <jaisi04@users.noreply.github.com>
2021-04-15 00:14:05 +0530

    adding fonts, icons & updating nav styles

--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,5 @@
 export const COLOR_YELLOW = '#F2B331';
 export const COLOR_BLACK = '#333333';
-export const COLOR_BLUE = '#0086E6';
-export const COLOR_REAL_BLUE = '#0000DD';
+export const COLOR_BLUE = '#20639B';
+export const COLOR_REAL_BLUE = '#17355F';
+export const COLOR_LIGHT_GRAY = '#E9E9E9';
--- src/components/Common/Skins.js
+++ src/components/Common/Skins.js
@@ -1,17 +1,36 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { COLOR_BLUE, COLOR_REAL_BLUE } from '../../colors';
+import { COLOR_LIGHT_GRAY, COLOR_BLUE, COLOR_REAL_BLUE } from '../../colors';
+
+export const StyledHeader = styled.header`
+  background-color: ${COLOR_LIGHT_GRAY};
+`;
 
 export const NavWrapper = styled.section`
   display: flex;
   flex-wrap: wrap;
+  width: 100%;
   justify-content: space-between;
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
 `;
 
 export const StyledLink = styled(Link)`
-  color: ${(props) => (props.active ? COLOR_BLUE : COLOR_REAL_BLUE)};
-  text-decoration: ${(props) => (props.active ? 'underline' : 'none')};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${(props) => (props.active ? COLOR_REAL_BLUE : COLOR_BLUE)};
+  text-decoration: none;
   font-size: 1.2em;
-  margin: 0 8px;
+  margin: 8px;
   font-weight: 500;
+  > i {
+    text-decoration: ${(props) => (props.active ? 'underline' : 'none')};
+    margin-right: 4px;
+  }
+  @media (max-width: 640px) {
+    width: 100%;
+    flex-direction: row;
+  }
 `;
